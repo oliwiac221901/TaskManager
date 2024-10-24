f0ef3df1ac648dafb80b28cb22521a070e83fa44
oliwiac221901 <oliwia.c221901@gmail.com>
2024-10-24 21:20:00 +0200

    Configure EntityFrameworkCore

---
+++ TaskManagerAPI/Core/TaskManagerAPI.Domain/Entities/UserManage/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerAPI.Domain.Entities.UserManage
+{
+	public class User
+	{
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
+
